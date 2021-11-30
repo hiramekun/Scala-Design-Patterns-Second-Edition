d319b093f5040bac7b01b8393e2a70d7884427d7
hiramekun <thescript1210@gmail.com>
2021-12-01 00:45:56 +0900

    Resolve warnings

--- Chapter08/sbt/src/main/scala/com/ivan/nikolov/behavioral/mediator/Student.scala
+++ Chapter08/sbt/src/main/scala/com/ivan/nikolov/behavioral/mediator/Student.scala
@@ -1,7 +1,6 @@
 package com.ivan.nikolov.behavioral.mediator
 
-import scala.collection.mutable.Map
-import scala.collection.mutable.Set
+import scala.collection.mutable
 
 trait Notifiable {
   def notify(message: String)
@@ -17,43 +16,43 @@ case class Group(name: String)
 
 trait Mediator {
   def addStudentToGroup(student: Student, group: Group)
-  
+
   def isStudentInGroup(student: Student, group: Group): Boolean
-  
+
   def removeStudentFromGroup(student: Student, group: Group)
-  
+
   def getStudentsInGroup(group: Group): List[Student]
-  
+
   def getGroupsForStudent(student: Student): List[Group]
-  
+
   def notifyStudentsInGroup(group: Group, message: String)
 }
 
 class School extends Mediator {
-  val studentsToGroups: Map[Student, Set[Group]] = Map()
-  val groupsToStudents: Map[Group, Set[Student]] = Map()
+  val studentsToGroups: mutable.Map[Student, mutable.Set[Group]] = mutable.Map()
+  val groupsToStudents: mutable.Map[Group, mutable.Set[Student]] = mutable.Map()
 
   override def addStudentToGroup(student: Student, group: Group): Unit = {
-    studentsToGroups.getOrElseUpdate(student, Set()) += group
-    groupsToStudents.getOrElseUpdate(group, Set()) += student
+    studentsToGroups.getOrElseUpdate(student, mutable.Set()) += group
+    groupsToStudents.getOrElseUpdate(group, mutable.Set()) += student
   }
 
-  override def isStudentInGroup(student: Student, group: Group): Boolean = 
-    groupsToStudents.getOrElse(group, Set()).contains(student) && studentsToGroups.getOrElse(student, Set()).contains(group)
+  override def isStudentInGroup(student: Student, group: Group): Boolean =
+    groupsToStudents.getOrElse(group, mutable.Set()).contains(student) && studentsToGroups.getOrElse(student, mutable.Set()).contains(group)
 
-  override def getStudentsInGroup(group: Group): List[Student] = 
-    groupsToStudents.getOrElse(group, Set()).toList
+  override def getStudentsInGroup(group: Group): List[Student] =
+    groupsToStudents.getOrElse(group, mutable.Set()).toList
 
-  override def getGroupsForStudent(student: Student): List[Group] = 
-    studentsToGroups.getOrElse(student, Set()).toList
+  override def getGroupsForStudent(student: Student): List[Group] =
+    studentsToGroups.getOrElse(student, mutable.Set()).toList
 
   override def notifyStudentsInGroup(group: Group, message: String): Unit = {
-    groupsToStudents.getOrElse(group, Set()).foreach(_.notify(message))
+    groupsToStudents.getOrElse(group, mutable.Set()).foreach(_.notify(message))
   }
 
   override def removeStudentFromGroup(student: Student, group: Group): Unit = {
-    studentsToGroups.getOrElse(student, Set()) -= group
-    groupsToStudents.getOrElse(group, Set()) -= student
+    studentsToGroups.getOrElse(student, mutable.Set()) -= group
+    groupsToStudents.getOrElse(group, mutable.Set()) -= student
   }
 }
 
@@ -68,26 +67,26 @@ object SchoolExample {
     val group1 = Group("Scala design patterns")
     val group2 = Group("Databases")
     val group3 = Group("Cloud computing")
-    
+
     school.addStudentToGroup(student1, group1)
     school.addStudentToGroup(student1, group2)
     school.addStudentToGroup(student1, group3)
-    
+
     school.addStudentToGroup(student2, group1)
     school.addStudentToGroup(student2, group3)
 
     school.addStudentToGroup(student3, group1)
     school.addStudentToGroup(student3, group2)
-    
+
     // notify
     school.notifyStudentsInGroup(group1, "Design patterns in Scala are amazing!")
-    
+
     // see groups
     System.out.println(s"$student3 is in groups: ${school.getGroupsForStudent(student3)}")
     // remove from group
     school.removeStudentFromGroup(student3, group2)
     System.out.println(s"$student3 is in groups: ${school.getGroupsForStudent(student3)}")
-    
+
     // see students in group
     System.out.println(s"Students in $group1 are ${school.getStudentsInGroup(group1)}")
   }
