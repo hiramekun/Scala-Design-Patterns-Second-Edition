a8987c4ca4b5ee0eccdde5d88077bf3c67d161e0
hiramekun <thescript1210@gmail.com>
2022-04-24 09:19:31 +0900

    optimize imports and reformat code

--- Chapter11/sbt/src/main/scala/com/ivan/nikolov/lens/User.scala
+++ Chapter11/sbt/src/main/scala/com/ivan/nikolov/lens/User.scala
@@ -1,11 +1,15 @@
 package com.ivan.nikolov.lens
 
-import scalaz.{LensFamily, Lens}
+import scalaz.Lens
 
 case class Country(name: String, code: String)
+
 case class City(name: String, country: Country)
+
 case class Address(number: Int, street: String, city: City)
+
 case class Company(name: String, address: Address)
+
 case class User(name: String, company: Company, address: Address)
 
 object User {
@@ -13,34 +17,34 @@ object User {
     (u, company) => u.copy(company = company),
     _.company
   )
-  
+
   val userAddress = Lens.lensu[User, Address](
     (u, address) => u.copy(address = address),
     _.address
   )
-  
+
   val companyAddress = Lens.lensu[Company, Address](
     (c, address) => c.copy(address = address),
     _.address
   )
-  
+
   val addressCity = Lens.lensu[Address, City](
     (a, city) => a.copy(city = city),
     _.city
   )
-  
+
   val cityCountry = Lens.lensu[City, Country](
     (c, country) => c.copy(country = country),
     _.country
   )
-  
+
   val countryCode = Lens.lensu[Country, String](
     (c, code) => c.copy(code = code),
     _.code
   )
-  
+
   val userCompanyCountryCode = userCompany >=> companyAddress >=> addressCity >=> cityCountry >=> countryCode
-  
+
   val userCompanyCountryCodeCompose = countryCode <=< cityCountry <=< addressCity <=< companyAddress <=< userCompany
 }
 
@@ -50,14 +54,14 @@ object UserVerboseExample {
     val london = City("London", uk)
     val buckinghamPalace = Address(1, "Buckingham Palace Road", london)
     val castleBuilders = Company("Castle Builders", buckinghamPalace)
-    
+
     val switzerland = Country("Switzerland", "CH")
     val geneva = City("geneva", switzerland)
     val genevaAddress = Address(1, "Geneva Lake", geneva)
-    
+
     val ivan = User("Ivan", castleBuilders, genevaAddress)
     System.out.println(ivan)
-    
+
     System.out.println("Capitalize UK code...")
 
     val ivanFixed = ivan.copy(
@@ -76,6 +80,7 @@ object UserVerboseExample {
 }
 
 object UserLensExample {
+
   import User._
 
   def main(args: Array[String]): Unit = {
@@ -92,9 +97,9 @@ object UserLensExample {
     System.out.println(ivan)
 
     System.out.println("Capitalize UK code...")
-    
+
     val ivanFixed = userCompanyCountryCode.mod(_.toUpperCase, ivan)
     System.out.println(ivanFixed)
   }
-  
+
 }
